a865bb5b23ad61c314db9df6ba8de58b31c210f0
Alan <alan_souza_nonijana@hotmail.com.br>
2020-08-31 17:59:43 -0300

    mvc inicio

---
+++ project-one/views/site/shared/layout.php
@@ -0,0 +1,20 @@
+<?php 
+	$url = $this->helpers['URLHelper']->getURL();
+	$location = $this->helpers['URLHelper']->getLocation();
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<meta name="keywords"	 content="HTML, CSS, JS, PHP">
+		<meta name="description" content="Descrição do site">
+		<meta name="author"		 content="Code Universe">
+		<meta name="viewport"	 content="width=device-width, initial-scale=1.0">
+		<meta name="robots"		 content="index, follow">
+		<title>Project One</title>
+	</head>
+	<body>
+
+	</body>
+</html>
